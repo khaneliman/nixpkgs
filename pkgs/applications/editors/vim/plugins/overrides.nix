7c6762b96f6d77effc1d9d7481ff3a947178feb6
Austin Horstman <khaneliman12@gmail.com>
2025-08-30 14:35:45 -0500

    vimPlugins.fzfWrapper: move to non-generated
    Looks like it was missed in `overrides.nix` when it should be in
    non-generated.
    
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- pkgs/applications/editors/vim/plugins/overrides.nix
+++ pkgs/applications/editors/vim/plugins/overrides.nix
@@ -1324,17 +1324,6 @@ in
     dependencies = [ self.fzfWrapper ];
   };
 
-  # Mainly used as a dependency for fzf-vim. Wraps the fzf program as a vim
-  # plugin, since part of the fzf vim plugin is included in the main fzf
-  # program.
-  fzfWrapper = buildVimPlugin {
-    inherit (fzf) src version;
-    pname = "fzf";
-    postInstall = ''
-      ln -s ${fzf}/bin/fzf $target/bin/fzf
-    '';
-  };
-
   gen-nvim = super.gen-nvim.overrideAttrs {
     runtimeDeps = [ curl ];
   };
