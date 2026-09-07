ab24880e06d9982066544c111b01ca452e9d3a4d
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:25 +0700

    python3Packages.static3: modernize

--- pkgs/development/python-modules/static3/default.nix
+++ pkgs/development/python-modules/static3/default.nix
@@ -13,15 +13,17 @@
   webtest,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "static3";
   version = "0.7.0";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchFromGitHub {
     owner = "rmohr";
     repo = "static3";
-    rev = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-uFgv+57/UZs4KoOdkFxbvTEDQrJbb0iYJ5JoWWN4yFY=";
   };
 
@@ -41,14 +43,14 @@ buildPythonPackage rec {
     pytest-cov-stub
     webtest
   ]
-  ++ lib.concatAttrValues optional-dependencies;
+  ++ lib.concatAttrValues finalAttrs.passthru.optional-dependencies;
 
   meta = {
-    changelog = "https://github.com/rmohr/static3/releases/tag/v${version}";
+    changelog = "https://github.com/rmohr/static3/releases/tag/${finalAttrs.src.tag}";
     description = "Really simple WSGI way to serve static (or mixed) content";
     mainProgram = "static";
     homepage = "https://github.com/rmohr/static3";
     license = lib.licenses.lgpl21Only;
     maintainers = with lib.maintainers; [ hexa ];
   };
-}
+})
