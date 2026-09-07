048156ab0e8a7dded34e24988359db42ec432a76
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:21 +0700

    python3Packages.dbus-client-gen: migrate to pyproject
    As part of https://github.com/NixOS/nixpkgs/issues/515974

--- pkgs/development/python-modules/dbus-client-gen/default.nix
+++ pkgs/development/python-modules/dbus-client-gen/default.nix
@@ -2,17 +2,20 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "dbus-client-gen";
   version = "0.5.1";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
     inherit pname version;
     hash = "sha256-vRXo72aWoreH/VwzdEAOgoGSRzRf7vy8Z/IA+lnLoWw=";
   };
+
+  build-system = [ setuptools ];
 
   meta = {
     description = "Python Library for Generating D-Bus Client Code";
