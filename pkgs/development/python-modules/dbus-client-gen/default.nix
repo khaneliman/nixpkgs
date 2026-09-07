6a680ebec254b6b0578e29fe8d48770f55d14ad3
Harinn <prinn.dev@pm.me>
2026-09-07 18:39:21 +0700

    python3Packages.dbus-client-gen: modernize

--- pkgs/development/python-modules/dbus-client-gen/default.nix
+++ pkgs/development/python-modules/dbus-client-gen/default.nix
@@ -5,17 +5,21 @@
   setuptools,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "dbus-client-gen";
   version = "0.5.1";
   pyproject = true;
 
+  __structuredAttrs = true;
+
   src = fetchPypi {
-    inherit pname version;
+    inherit (finalAttrs) pname version;
     hash = "sha256-vRXo72aWoreH/VwzdEAOgoGSRzRf7vy8Z/IA+lnLoWw=";
   };
 
   build-system = [ setuptools ];
+
+  pythonImportsCheck = [ "dbus_client_gen" ];
 
   meta = {
     description = "Python Library for Generating D-Bus Client Code";
@@ -23,4 +27,4 @@ buildPythonPackage rec {
     license = lib.licenses.mpl20;
     maintainers = with lib.maintainers; [ nickcao ];
   };
-}
+})
